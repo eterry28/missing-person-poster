acbb5aac7bb1220f58469117b4eed367cbcb2dd5
Eric Terry <eric.terry.kc@gmail.com>
2023-02-16 15:49:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+{
+  
+
+  $(".header").innerHTML = "MISSING PERSON";
+  $(".name").innerHTML = "PERSON'S NAME";
+
+  $(".location").innerHTML = "CITY, STATE";
+  $(".date").innerHTML = "MONTH 00, 2021";
+
+
+$(".dob").innerHTML = "Month 00, 2000";
+$(".eyes").innerHTML = "Brown";
+$(".hair").innerHTML = "Black, Long";
+$(".weight").innerHTML = "60 to 70 pounds";
+$(".height").innerHTML = "3'0\"";
+$(".sex").innerHTML = "Female";
+$(".race").innerHTML = "Hispanic";
+
+
+$(".footer").innerHTML = "CALL (555) 555 - 1234";
+}
